0b39b750205d4590793f1b2ad3c207440775a861
facereplays <facereplays@gmail.com>
2022-03-11 10:54:23 +0300

    sdfg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { Hex2textComponent } from './hex2text/hex2text.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Hex2textComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule,MatTabsModule,MatGridListModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
