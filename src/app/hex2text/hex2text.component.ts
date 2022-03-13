2549322cee4182ab3e7a0e5a8f0827f2732d4c04
facereplays <facereplays@gmail.com>
2022-03-13 16:23:09 +0300

    loading files

--- src/app/hex2text/hex2text.component.ts
+++ src/app/hex2text/hex2text.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {FileReadService} from "./file-read.service";
 import {Clipboard} from '@angular/cdk/clipboard';
 
@@ -33,11 +33,23 @@ export class Hex2textComponent implements OnInit {
   clipboardContent: string;
   tabs: tab[];
   seleT: number;
+  private zone: NgZone;
+  copied = 'nov';
 
-  constructor(private fileService: FileReadService, private clipboard: Clipboard) {
+
+  constructor(private fileService: FileReadService, private clipboard: Clipboard, zone: NgZone) {
+    this.zone = zone;
   }
 
-  res(n: pair) {
+  /***
+   * mousedown action
+   * first cell selected
+   *
+   *
+   * @param n item
+   * @param t  item's type
+   */
+  res(n: pair, t:boolean) {
     this.resultHex.forEach(a => {
       a.act = ''
     });
@@ -46,29 +58,54 @@ export class Hex2textComponent implements OnInit {
     this.selecting = true;
   }
 
-  resu(nm) {
+  /****
+   * MouseUp event
+   * filling clipboard with data
+   * finalising selection
+   *
+   * @param nm last touched pair
+   * @param t which side is touched 0=hex, 1=ascii
+   */
+  resu(nm: pair, t:boolean) {
     let tex = '';
     this.selecting = false;
     const sel = document.getSelection();
-    if (nm.pos > this.startSel) {
-      this.endSel = nm.pos;
+nm.act='act';
 
-    } else {
-      this.endSel = this.startSel;
-      this.startSel = nm.pos;
-    }
-    for (let i = this.startSel; i <= this.endSel; i++) {
-      this.resultHex[i].act = 'act';
-      tex += this.resultHex[i].hex + ' ';
-      // console.log(i);
-    }
-    let element: HTMLElement = document.getElementById('trigger') as HTMLElement;
-    element.click();
+    this.resultHex.forEach(a => {
+      const val= t ? a.ascii : a.hex;
+      if(a.act === 'act'){
+        tex += val + ' ';
+
+      }
+    });
+    /***
+     * style animation manipulation for clipboard
+     *
+     */
+    this.copied='nov ad'
+
+    this.zone.run(()=>{
+        setTimeout(()=>{
+          const w=1;
+          this.copied=' nov';},2000);
+}
+      );
+
+
     this.clipboardContent = tex;
     this.clipboard.copy(tex);
     sel.removeAllRanges();
   }
 
+  /***
+   *
+   * mouse over item
+   * collect activated selected cells
+   *
+   *
+   * @param nm
+   */
   mOver(nm) {
     if (this.selecting) {
       nm.act = 'act';
@@ -89,6 +126,13 @@ export class Hex2textComponent implements OnInit {
 
   }
 
+  /***
+   *
+   * reading content of file
+   *
+   *
+   * @param strns
+   */
   read(strns) {
     const str = strns[0];
     const myArr = new Uint8Array(str.length);
@@ -98,26 +142,35 @@ export class Hex2textComponent implements OnInit {
         this.resultHex.push({act: '', hex: str.charCodeAt(i).toString(16), pos: i, ascii: str[i]} as pair);
       } else {
         this.resultHex.push({act: '', hex: '00', pos: i, ascii: '.'} as pair);
-
-
-      }
+ }
     });
     const nt = new tab();
     nt.name = strns[1];
     nt.resulthex = this.resultHex;
+    /***
+     *
+     * add new tab with name of file
+     *
+     */
     this.tabs.push(nt);
+    /***
+     *
+     * activate new tab
+     *
+     */
     this.seleT = this.tabs.length - 1;
   }
 
-  resv(n) {
-    // console.log(n);
-
-  }
 
   ngOnInit(): void {
     this.seleT = 0;
     this.resultHex = [];
     this.hexaInp = '54 65 73 74 69 6e 67 20 68 65 78 20 63 6f 6d 70 6f 6e 65 6e 74 20 66 6f 72 20 63 6f 64 65 20 63 68 61 6c 6c 65 6e 67 65';
+  /***
+
+  reading first example file
+   */
+
     this.fileService.readFile('assets/test.svg').subscribe(
       str => {
 
@@ -136,6 +189,11 @@ export class Hex2textComponent implements OnInit {
 
 
         });
+        /***
+         *
+         *
+         * creating first tab
+         */
         this.tabs = [{name: 'test.svg', resulthex: this.resultHex} as tab];
 
 
@@ -153,7 +211,11 @@ export class Hex2textComponent implements OnInit {
     return str;
   }
 
-
+  /****
+   *
+   * disabling selecting process
+   *
+   */
   mouseUp() {
     this.selecting = false;
   }
